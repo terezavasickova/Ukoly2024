1513d0642e927ec5a41f89c5bc9fec7d7f56a350
terezavasickova <terka.vasickova03@gmail.com>
2025-02-12 21:04:30 +0100

    Update Kniha.py

--- Ukol_9/python/SystemKnihovny/Kniha.py
+++ Ukol_9/python/SystemKnihovny/Kniha.py
@@ -1,4 +1,4 @@
-///class Kniha:
+class Kniha:
     def __init__(self, nazev: str, autor: str, rok_vydani: int, isbn: str):
         self._nazev = nazev
         self._autor = autor
@@ -8,20 +8,5 @@
     # doplňte potřebné gettry a setry
 
     def __str__(self) -> str:
-        return ""///
+        return ""
         
-#import re
-
-class Kniha:
-    def __init__(self, nazev, autor, rok_vydani, isbn):
-        self.nazev = nazev
-        self.autor = autor
-        self.rok_vydani = rok_vydani
-        self.isbn = isbn
-        
-        if not self.validuj_isbn(isbn):
-            raise ValueError(f"ISBN '{isbn}' není platné.")
-
-    def validuj_isbn(self, isbn):
-        # Jednoduchá validace ISBN (např. kontrola délky a formátu)
-        return bool(re.match(r"^\d{13}$", isbn))
