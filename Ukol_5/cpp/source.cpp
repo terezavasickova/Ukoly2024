b99f5d3157b90ba1e1c2ff4c53a3784e3941825b
terezavasickova <terka.vasickova03@gmail.com>
2025-02-27 15:04:20 +0100

    Update source.cpp

--- Ukol_5/cpp/source.cpp
+++ Ukol_5/cpp/source.cpp
@@ -1,21 +1,14 @@
-// organismus.h
-#ifndef ORGANISMUS_H
-#define ORGANISMUS_H
+#include <iostream>
+#include <vector>
 
+//  třída Organismus
 class Organismus {
 public:
     virtual void aktualizuj() = 0;
     virtual ~Organismus() {}
 };
 
-#endif // ORGANISMUS_H
-
-// zvire.h
-#ifndef ZVIRE_H
-#define ZVIRE_H
-
-#include "organismus.h"
-
+//Třída Zvire (dědí z Organismus)
 class Zvire : public Organismus {
 protected:
     int energie;
@@ -24,104 +17,53 @@ public:
     virtual void pohyb() = 0;
 };
 
-#endif // ZVIRE_H
-
-// rostlina.h
-#ifndef ROSTLINA_H
-#define ROSTLINA_H
-
-#include "organismus.h"
-
+// Třída Rostlina (dědí z Organismus)
 class Rostlina : public Organismus {
 public:
-    void aktualizuj() override;
+    void aktualizuj() override {
+        std::cout << "Rostlina roste.\n";
+    }
 };
 
-#endif // ROSTLINA_H
-
-// bylozravec.h
-#ifndef BYLOZRAVEC_H
-#define BYLOZRAVEC_H
-
-#include "zvire.h"
-
+// Třída Bylozravec (dědí z Zvire)
 class Bylozravec : public Zvire {
 public:
     Bylozravec(int e) : Zvire(e) {}
-    void pohyb() override;
-    void aktualizuj() override;
+    void pohyb() override {
+        std::cout << "Býložravec se pohybuje.\n";
+    }
+    void aktualizuj() override {
+        std::cout << "Býložravec se krmí.\n";
+    }
 };
 
-#endif // BYLOZRAVEC_H
-
-// masozravec.h
-#ifndef MASOZRAVEC_H
-#define MASOZRAVEC_H
-
-#include "zvire.h"
-
+// Třída Masozravec (dědí z Zvire)
 class Masozravec : public Zvire {
 public:
     Masozravec(int e) : Zvire(e) {}
-    void pohyb() override;
-    void aktualizuj() override;
+    void pohyb() override {
+        std::cout << "Masožravec loví.\n";
+    }
+    void aktualizuj() override {
+        std::cout << "Masožravec se krmí.\n";
+    }
 };
 
-#endif // MASOZRAVEC_H
-
-// prostredi.h
-#ifndef PROSTREDI_H
-#define PROSTREDI_H
-
-#include <vector>
-#include "organismus.h"
-
+// Třída Prostredi (správa ekosystému)
 class Prostredi {
 private:
     std::vector<Organismus*> organismy;
 public:
-    void pridatOrganismus(Organismus* o);
-    void aktualizuj();
+    void pridatOrganismus(Organismus* o) {
+        organismy.push_back(o);
+    }
+    void aktualizuj() {
+        for (auto o : organismy) {
+            o->aktualizuj();
+        }
+    }
 };
 
-#endif // PROSTREDI_H
-
-// source.cpp
-#include "rostlina.h"
-#include "bylozravec.h"
-#include "masozravec.h"
-#include "prostredi.h"
-#include <iostream>
-
-void Rostlina::aktualizuj() {
-    std::cout << "Rostlina roste.\n";
-}
-
-void Bylozravec::pohyb() {
-    std::cout << "Býložravec se pohybuje.\n";
-}
-
-void Bylozravec::aktualizuj() {
-    std::cout << "Býložravec se krmí.\n";
-}
-
-void Masozravec::pohyb() {
-    std::cout << "Masožravec loví.\n";
-}
-
-void Masozravec::aktualizuj() {
-    std::cout << "Masožravec se krmí.\n";
-}
-
-void Prostredi::pridatOrganismus(Organismus* o) {
-    organismy.push_back(o);
-}
-
-void Prostredi::aktualizuj() {
-    for (auto o : organismy) {
-        o->aktualizuj();
-    }
-}
 
 int main() {
     Prostredi ekosystem;
